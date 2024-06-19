90929bcffb7c45d1f36cf5fcdce36cde61f7222c
dan <121939052+spncdan@users.noreply.github.com>
2024-06-19 17:26:29 -0400

    Create 01. Introduction.java

---
+++ 01. Introduction.java
@@ -0,0 +1,40 @@
+// Java is a general-purpose * powerful programming language
+// It is used for developing software that will run on mobile, desktop, and servers.
+// Machine independent
+
+// Syntax Error: Doesn't follow Java Rules
+// Semantic Error: Has No Meaning
+
+// API - Application Programming Interface
+// API is a library which contains predefined Java code that we can use to develop Java programs
+// This makes the development process faster and easier as there is no need to write everything from scratch
+
+// Java (SE) is what will be used for this learning process (acts as foundation for all other editions)
+
+// JDK - Java Development Kit
+// Set of programs that enable us to develop our programs
+// Contains JRE (Java Runtime Environment) which is used to run our programs
+// JVM (Java Virtual Machine) executes our java programs on different machines (making it independent)
+
+/* IDE (integrated development environment) allows us to
+
+    1. Write | Source Code
+    2. Compile | Machine Code
+    3. Debug | Tools to Find Errors
+    4. Build | Files that can be executed by JVM
+    5. Run | Execute program from within IDE
+
+*/
+
+// All of the above makes development faster and easier
+
+// Class - Blueprint to create Objects
+// Object - Instances of a Class
+
+// Here is how you are able to Print "Hello World" in Java
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
