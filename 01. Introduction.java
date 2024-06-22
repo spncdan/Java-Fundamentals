d88193816a2324937077f5f5029863bae264acd9
dan <121939052+spncdan@users.noreply.github.com>
2024-06-22 15:54:00 -0400

    Update 01. Introduction.java

--- 01. Introduction.java
+++ 01. Introduction.java
@@ -1,23 +1,29 @@
 // Welcome to my self-learning Java repository
 // I will be making notes here of my transition to understand Java after having learned core concepts of C++
 
-// Java is a general-purpose * powerful programming language
-// It is used for developing software that will run on mobile, desktop, and servers.
-// Machine independent
+/*
+Java is a general-purpose * powerful programming language
+It is used for developing software that will run on mobile, desktop, and servers.
+Machine independent
+*/
 
 // Syntax Error: Doesn't follow Java Rules
 // Semantic Error: Has No Meaning
 
-// API - Application Programming Interface
-// API is a library which contains predefined Java code that we can use to develop Java programs
-// This makes the development process faster and easier as there is no need to write everything from scratch
+/*
+API - Application Programming Interface
+API is a library which contains predefined Java code that we can use to develop Java programs
+This makes the development process faster and easier as there is no need to write everything from scratch
+*/
 
 // Java (SE) is what will be used for this learning process (acts as foundation for all other editions)
 
-// JDK - Java Development Kit
-// Set of programs that enable us to develop our programs
-// Contains JRE (Java Runtime Environment) which is used to run our programs
-// JVM (Java Virtual Machine) executes our java programs on different machines (making it independent)
+/*
+JDK - Java Development Kit
+Set of programs that enable us to develop our programs
+Contains JRE (Java Runtime Environment) which is used to run our programs
+JVM (Java Virtual Machine) executes our java programs on different machines (making it independent)
+*/
 
 /* IDE (integrated development environment) allows us to
 
@@ -31,21 +37,41 @@
 
 // All of the above makes development faster and easier
 
-// Class - Blueprint to create Objects
-// Object - Instances of a Class
+/*
+ Class - Blueprint to create Objects (Container of methods)
+ Object - Instances of a Class
+ Method - Groups of instructions to do specific tasks (Every method is written inside a class)
+*/
+
+// In Java, you end statements with ; similar to C++
+
+/* the main() method is automatically called when we run our java program
+ 1. First method that is called
+ 2. Starting point of execution of our program
+*/
+
+/* Some important Java Access Keyword
+- Public
+- Private
+- Protected
+- Default
+- Static
+*/
+
+/* Naming Conventions
+Pascal Case Convention -> ThisIsAName (All first letters of word are capitalized) (Naming Classes)
+Camel Case Convention -> thisIsAName (First letter of first word is not capitalized) (Naming Variables and Methods)
+Snake Case Convention -> this_is_a_name
+*/
+
+// Structure of Java Program
 
 // Class structures are as follows:
-
-class class_name {
-    // Code block - Code within brackets
-}
-
-// Method - are groups of instructions to do specific tasks
-
-// Here is how you are able to Print "Hello World" in Java
-
 public class Main {
+
+    // Method structures are as follows:
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // Outputting "Hello World" into the terminal
     }
+
 }
