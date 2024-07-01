7d7cc845cb9ccdbe662b156030355a3c68e2ab8c
dan <121939052+spncdan@users.noreply.github.com>
2024-07-01 17:25:50 -0400

    Add files via upload

---
+++ 01. Introduction/01. Introduction.java
@@ -0,0 +1,89 @@
+// Welcome to my self-learning Java repository
+// I will be making notes here of my transition to understand Java after having learned core concepts of C++
+
+/*
+Java is a general-purpose * powerful programming language
+It is used for developing software that will run on mobile, desktop, and servers.
+Machine independent
+*/
+
+// Syntax Error: Doesn't follow Java Rules
+// Semantic Error: Has No Meaning
+
+/*
+API - Application Programming Interface
+API is a library which contains predefined Java code that we can use to develop Java programs
+This makes the development process faster and easier as there is no need to write everything from scratch
+*/
+
+// Java (SE) is what will be used for this learning process (acts as foundation for all other editions)
+
+/*
+JDK - Java Development Kit
+Set of programs that enable us to develop our programs
+Contains JRE (Java Runtime Environment) which is used to run our programs
+JVM (Java Virtual Machine) executes our java programs on different machines (making it independent)
+*/
+
+/* IDE (integrated development environment) allows us to
+
+    1. Write | Source Code
+    2. Compile | Machine Code
+    3. Debug | Tools to Find Errors
+    4. Build | Files that can be executed by JVM
+    5. Run | Execute program from within IDE
+
+*/
+
+// All of the above makes development faster and easier
+
+/*
+ Class - Blueprint to create Objects (Container of methods)
+ Object - Instances of a Class
+ Method - Groups of instructions to do specific tasks (Every method is written inside a class)
+*/
+
+// In Java, you end statements with ; similar to C++
+
+/* the main() method is automatically called when we run our java program
+ 1. First method that is called
+ 2. Starting point of execution of our program
+*/
+
+/* Some important Java Access Keyword
+- Public
+- Private
+- Protected
+- Default
+- Static
+*/
+
+/* Naming Conventions
+Pascal Case Convention -> ThisIsAName (All first letters of word are capitalized) (Naming Classes)
+Camel Case Convention -> thisIsAName (First letter of first word is not capitalized) (Naming Variables and Methods)
+Snake Case Convention -> this_is_a_name
+*/
+
+// Structure of Java Program
+
+// Class structures are as follows:
+public class Main {
+
+    // Method structures are as follows:
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // Code within method which outputs "Hello World" into the terminal
+    }
+
+}
+
+// Packages are containers for many different classes
+
+/* Main Points to Remember:
+
+1. Every Java program contains at least one Class.
+2. Pascal case is used with classes.
+3. Camel case is used with methods.
+4. A method exists inside a class.
+5. main() is the starting point of execution of our program.
+
+*/
